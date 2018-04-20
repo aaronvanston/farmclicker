ff3035656e8540150d17a5082c8be145e59165dc
Aaron Vanston <aaron.vanston@rea-group.com>
2018-04-20 19:07:31 +1000

    feat: set-up initial react

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const rootEl = document.getElementById('root');
+
+const App = () => <div>Hello World</div>
+
+ReactDOM.render(
+  <App />,
+  rootEl,
+);
