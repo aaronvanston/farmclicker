2f75c3057efcd4b36a99eeeb543fc2f779778513
Aaron Vanston <aaron.vanston@rea-group.com>
2018-04-24 10:07:54 +1000

    refactor: card styles

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -3,9 +3,15 @@ import React from 'react';
 import styles from './ProductCard.css';
 
 const ProductCard = ({ name, quantity }) => (
-  <div className={styles.card}>
-    <div className={styles.name}>{name}</div>
-    <span className={styles.name}>{quantity}</span>
+  <div className={styles.card} role="button">
+    <div className={styles.body}>
+      <div className={styles.name}>[{name}]</div>
+      <span className={styles.quantity}>{quantity}</span>
+    </div>
+    <div className={styles.actionBar}>
+      <button className={styles.actionBtn}>Collect</button>
+      <button className={styles.actionBtn}>Sell</button>
+    </div>
   </div>
 )
 
