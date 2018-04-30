26fa76b8fac06e247d7d7f164ca13c34663a5622
Aaron Vanston <aaron.vanston@rea-group.com>
2018-04-30 21:43:53 +1000

    refactor: remove debug set-up

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,23 +13,20 @@ import Shop from '~/components/Shop/Shop';
 
 import styles from './App.css';
 
-const App = ({
-  products, producers, inventory, handleAddProducer, handleAddProduct, handleSellProduct,
-}) => (
+const App = ({products, inventory}) => (
   <div className={styles.wrapper}>
     <main className={styles.main}>
       <Header total={inventory.totalMoney} />
+
       <ProductGrid>
         {products.list.map(product => (
-          <ProductCard key={product} name={product} quantity={products.quantity[product]} />
+          <ProductCard
+            key={product}
+            name={product}
+            quantity={products.quantity[product]}
+          />
         ))}
       </ProductGrid>
-      
-      <div>
-        Producer:
-        <button onClick={() => handleAddProducer(constants.producers.CHICKEN, 1)}>Add Chicken</button>
-        <button onClick={() => handleAddProducer(constants.producers.COW, 1)}>Add Cow</button>
-      </div>
     </main>
 
     <aside className={styles.aside}>
@@ -41,14 +38,7 @@ const App = ({
 
 const mapStateToProps = state => ({
   products: state.products,
-  producers: state.producers,
   inventory: state.inventory,
 });
 
-const mapDispatchToProps = dispatch => ({
-  handleAddProducer: (name, price) => { dispatch(addProducer(name, price)); },
-  handleAddProduct: (name, amount) => { dispatch(addProduct(name, amount)); },
-  handleSellProduct: (name) => { dispatch(sellProduct(name)); },
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps)(App);
